ba2a190d83ae3e5cf79aecae4533e8329670ad11
Ethan Romm <ejromm@gmail.com>
2023-02-08 21:57:01 -0500

    updated home-page with map

--- src/homepage.js
+++ src/homepage.js
@@ -37,14 +37,35 @@ function homePage() {
 
     // main content
 
-    const mainBottom = document.createElement('div'); 
-    mainBottom.classList.add('main-content'); 
-    container.appendChild(mainBottom); 
+    const mainImageDiv = document.createElement('div'); 
+    mainImageDiv.classList.add('main-content'); 
+    container.appendChild(mainImageDiv); 
 
     const mainImg = new Image(); 
     mainImg.src = Icon; 
     mainImg.classList.add('main-image'); 
-    mainBottom.appendChild(mainImg); 
+    mainImageDiv.appendChild(mainImg); 
+
+    //location content
+
+    const locationDiv = document.createElement('div'); 
+    locationDiv.classList.add('location'); 
+    mainImageDiv.appendChild(locationDiv); 
+
+    function initMap() {
+        const mapDiv= document.createElement('div'); 
+        mapDiv.classList.add('map'); 
+        locationDiv.appendChild(mapDiv)
+      
+        mapDiv.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d388616.6560445083!2d-74.19946370213873!3d40.451640191993164!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd134e199a405a163!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1675910989646!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`; 
+        
+
+
+    
+    }
+    initMap(); 
+    
+
     
 
 }
